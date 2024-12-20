564d900549039583b1efd7785a33502c702b4bbe
Aditya Bhalgat <adityabhalgat@Adityas-MacBook-Air.local>
2024-12-20 12:16:30 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignupPage'
+
+const App = () => {
+  return (
+    <div>
+      <SignUpPage/>
+    </div>
+  )
+}
+
+export default App
