f760ba2dca523380d1e19925839b32163ff393fa
Aditya Bhalgat <adityabhalgat@Adityas-MacBook-Air.local>
2024-12-24 17:25:32 +0530

    Added the dashboards of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignupPage'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserDashboard from "./pages/UserDashboard";
+import UserAppointments from "./pages/UserAppointments";
+import UserReminders from "./pages/UserReminder";
+import UserBills from "./pages/UserBills";
+// import UserMessages from "./pages/UserMessages";
+import DoctorDashboard from "./pages/DoctorDashboard"; // Doctor's Dashboard
+import DoctorAppointments from "./pages/DoctorAppointments"; // Doctor's Appointments
+// import DoctorMessages from "./pages/DoctorMessages"; // Doctor's Messages
 
 const App = () => {
   return (
-    <div>
-      <SignUpPage/>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        {/* User Routes */}
+        <Route path="/user/dashboard" element={<UserDashboard />} />
+        <Route path="/user/appointments" element={<UserAppointments />} />
+        <Route path="/user/reminders" element={<UserReminders />} />
+        <Route path="/user/bills" element={<UserBills />} />
+        {/* <Route path="/user/messages" element={<UserMessages />} /> */}
 
-export default App
+        {/* Doctor Routes */}
+        <Route path="/admin/dashboard" element={<DoctorDashboard />} />
+        <Route path="/admin/appointments" element={<DoctorAppointments />} />
+        {/* <Route path="/admin/messages" element={<DoctorMessages />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
